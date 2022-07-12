435365e5c637660d76cb14e582d315f649bf6695
maricoIR <matin.kahrizi@gmail.com>
2022-07-12 14:21:58 +0430

    Changing a Project's Name

--- src/views/Home/components/Projects/Projects.js
+++ src/views/Home/components/Projects/Projects.js
@@ -49,9 +49,7 @@ const Projects = () => {
       <div className="flex flex-col items-start gap-3 cursor-pointer">
         <div className="bg-[#383b92] z-[100] w-[400px] h-[300px] relative rounded-md">
           <div className="absolute bottom-0 w-full h-14 bg-[#192a3c] flex items-center justify-center rounded-bl-md rounded-br-md">
-            <span className="text-white font-[600] text-[18px]">
-              Inventory App
-            </span>
+            <span className="text-white font-[600] text-[18px]">Quiz App</span>
           </div>
         </div>
       </div>
