0c5025eadbe920edf2b615b58a5bd6294e99d041
maricoIR <matin.kahrizi@gmail.com>
2022-07-20 16:10:11 +0430

    Responsive Issues Fixed

--- src/views/Home/components/Projects/SingleProject.js
+++ src/views/Home/components/Projects/SingleProject.js
@@ -1,7 +1,7 @@
 const SingleProject = (props) => {
   return (
     <div
-      className="flex flex-col items-start gap-3 cursor-pointer"
+      className="flex flex-col md:items-start items-center w-full gap-3 cursor-pointer"
       onClick={() => {
         props.setModal(true);
         props.setModalBody([
@@ -13,7 +13,7 @@ const SingleProject = (props) => {
         ]);
       }}
     >
-      <div className="gradient z-[100] w-[400px] h-[300px] relative rounded-md flex items-center justify-center overflow-hidden">
+      <div className="gradient z-[100] md:w-[400px] w-[85vw] h-[300px] relative rounded-md flex items-center justify-center overflow-hidden">
         <div className="absolute bg-[#ffffffa1] rounded-bl-md rounded-br-md top-0 left-1/2 translate-x-[-50%] w-[300px] h-6 flex items-center justify-center">
           <span className="text-slate-600 text-[13.5px] font-semibold">
             Click to see project's details
